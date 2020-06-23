80e057c705f2b9544ac712662d1b860ef46675fc
Khaled Yassen <40730865+KayvT@users.noreply.github.com>
2020-06-23 17:12:29 +0300

    added read me

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var socket = require("socket.io");
 
 var app = express();
-var server = app.listen(2000, function() {});
+var server = app.listen(2000, function () {});
 
 //Static files
 app.use(express.static("public/")); //I followed the convetion we saw in the video. We do not need this but it is good to have the stuff inside a file.
@@ -13,37 +13,31 @@ var io = socket(server);
 const BOT_NAME = "Asem"; // just test names
 const PERSON_ONE_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
 
-
 // name socket array
 arrNameSocket = require("./socketManager").name_socket;
 
-
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
   var userName;
   console.log("Someone just logged in!", "id", socket.id);
 
   // Getting the emmitted name and pushing it along with the socket to the array
-  socket.on('nameInfo', function(data){
+  socket.on("nameInfo", function (data) {
     userName = data.name;
-    nameIsValid = arrNameSocket.some(obj => obj.name === data.name);
-    io.sockets.emit('validateUser', nameIsValid);
+    nameIsValid = arrNameSocket.some((obj) => obj.name === data.name);
+    io.sockets.emit("validateUser", nameIsValid);
     if (!nameIsValid)
       arrNameSocket.push({ name: data.name, socketId: socket.id });
-  })
+  });
 
   // handling the emmitted data from msgForm( with event 'chat')
-  socket.on("chat", function(data) {
-    data = {name:data.name, socketId:socket.id, message:data.message}
-    io.to(socket.id).emit("chat-me", data); // emit to my socket to disp my message on the right side 
+  socket.on("chat", function (data) {
+    data = { name: data.name, socketId: socket.id, message: data.message };
+    io.to(socket.id).emit("chat-me", data); // emit to my socket to disp my message on the right side
     socket.broadcast.emit("chat", data); // emit to all and disp on left
-   
   });
 
-  socket.on('disconnect', function () {
-    arrNameSocket = arrNameSocket.filter(person => person.name != userName);
+  socket.on("disconnect", function () {
+    arrNameSocket = arrNameSocket.filter((person) => person.name != userName);
     // console.log(arrNameSocket)
   });
-
-
 });
-
